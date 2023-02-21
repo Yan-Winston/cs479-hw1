3873520450be4af790d474b84d2d67de66f73548
Yan-Winston <wyan@Winstons-MacBook-Pro.local>
2023-02-20 23:10:00 -0800

    box muller done

--- driver.cpp
+++ driver.cpp
@@ -4,9 +4,76 @@
 
 
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
 #include "classifier.h"
 
 
+
+struct Coordinate{
+    double x, y;
+    int actual_class; //either 1 or 2
+};
+
+double ranf() {
+    return (rand()/(double)RAND_MAX);
+}
+
+double box_muller(double m, double s) {
+    double x1, x2, w, y1;
+    static double y2;
+    static bool use_last = false;
+
+    if (use_last) {
+        y1 = y2;
+        use_last = false;
+    } else {
+        do {
+            x1 = 2*ranf() - 1;
+            x2 = 2*ranf() - 1;
+            w = x1 * x1 + x2 * x2;
+        } while (w >= 1.0);
+
+        w = sqrt((-2*log(w))/w);
+
+        y1 = x1 * w;
+        y2 = x2 * w;
+        use_last = true;
+    }
+
+    return(m+y1*s);
+}
+
+void generateData() {
+    std::ofstream f;
+    f.open("testData.csv", std::ios::out);
+
+    Coordinate tempCoord;
+
+    for (int i = 0; i < 60000; i++) { // N(µ1, ∑1)
+        tempCoord.x = -1;
+        tempCoord.x = -1;
+        tempCoord.actual_class = 1;
+
+
+
+        //f << ;
+    }
+
+    for (int i = 0; i < 140000; i++) {
+        tempCoord.x = -1;
+        tempCoord.y = -1;
+        tempCoord.actual_class = 2;
+
+        f << 3;
+    }
+    f.close();
+}
+
+void trainAndTest() {
+
+}
 
 int menu() {
     std::string choice;
@@ -22,14 +89,6 @@ int menu() {
     std::cout << std::endl << std::endl;
 
     return std::stoi(choice);
-}
-
-void generateData() {
-
-}
-
-void trainAndTest() {
-
 }
 
 
